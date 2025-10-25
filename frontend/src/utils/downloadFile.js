f457a5ed4e514da9c465eee636af5dd4c540cdda
Jean-David Pallares <jeandavid.pallares@gmail.com>
2025-10-25 00:38:55 -0400

    feat: add file download utility for CSV/JSON exports

---
+++ frontend/src/utils/downloadFile.js
@@ -0,0 +1,9 @@
+// Helps download exported DB files
+export default function downloadFile(data, filename, mimeType = "text/plain") {
+  const blob = new Blob([data], { type: mimeType });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  a.click();
+}
